f8771791e3507bb7ec32fdcc62abc249a5199f33
paulyiu312 <paulyiuchiho@gmail.com>
2022-02-13 16:16:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://github.com/hms5232/random-survey';
+
+if (urls.length > 0){
+  window.location.href = urls[Math.floor(Math.random() * urls.length)]
+}
+  
