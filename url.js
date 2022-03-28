2503559025c17da32ad1f17f695cd0e467e95329
paulyiu312 <paulyiuchiho@gmail.com>
2022-03-28 15:33:05 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,13 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/1f-p3BovrJh3hyLzyeEfS7qVBt8uB-ax7TRAixj6WScI/edit',
-	'https://docs.google.com/forms/d/11SvT4teZ4jHGF75jH5qOOf9vUtQtLwbhVgOzSRW3Ycg/viewform?edit_requested=true'
+	'https://docs.google.com/forms/d/1r6dK28gHNnCQTD3iniqVeSydMmn6TOe69cVxfitnXWE/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1p62cFR5QMqaeOG4Z2MY_x3oH7TETIdnaBAU46wa_gJk/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1ivG-jC78bFAFPdkEjRd87B2VzS0D29EcmSE6o8P-wms/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1a40_KY1ECqO_QjoCokSo-Z-0ye7vRiKAOk1PVvH2Ago/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1T3N_9wmj12M3rPcdsZMChRtBjtIIIHiFY6BrFuDULfI/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1MaQ5PZ0vi1rBqS9IGkniBo296RWQleT62aaSrbiBaXE/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/1J0bVeS6Oz4QW2IDGA9cdcWvSw9Wv9l6Vu7cf2w2ny3E/viewform?edit_requested=true',
+	'https://docs.google.com/forms/d/115TIHeVI7n9ZCdmHxH9eS2dLbnTLLe-4lL5V0xcq_wQ/viewform?edit_requested=true',
 ];
 
