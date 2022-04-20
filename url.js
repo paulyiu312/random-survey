351da3a0c88d4558c179b272a3d0cc73883d718a
paulyiu312 <paulyiuchiho@gmail.com>
2022-04-20 17:20:29 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,8 +11,6 @@ const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLScDIAgk_eOYPNfs3fbtuD6vLP5KXqfde8rL8grMTqxU7qjrEw/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLScuJUqzzmIPAUKRGApuk7yyEWwhdPvRuuS4g5DxosStM4rMlA/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLSdLsDmhxQaYQifmT0mMMOn93GPOfsiTFZ-mlV789yoHg8Odtw/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSfHDwKcsrp5BYfBLpq4KoWYRYaGeHWPeelQvitjwyYckF5gjw/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSfhBJT052omPZokx-Nlkd6JYxU9TNwbq5-36DocyDzqeW_wnQ/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLSect682tPiJnZWP3-_NSxMKcaZ61jTwRB63ga2eiyvZuPYG5w/viewform?usp=sf_link',
 ];
 
